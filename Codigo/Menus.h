2a6a0af427216a6d533a86f0032cdf31a4207fda
nicolasmira101 <nicolasmiranda@javeriana.edu.co>
2020-05-27 15:05:12 -0500

    Actualización nombre de archivos

--- Codigo/Menus.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENUS_H
-#define MENUS_H
-
-void menuPrincipal( int & );
-void menuNuevoJuego( int & );
-
-#endif // MENUS_H
-
---
+++ Codigo/mmenus.h
@@ -0,0 +1,8 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+void menuPrincipal( int & );
+void menuNuevoJuego( int & );
+
+#endif // MENUS_H
+
